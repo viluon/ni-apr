d8368af3c4b0c8bdd4eb6036c6d1f44bfa1db3bf
Andrew Kvapil <viluon@pm.me>
2022-03-06 21:02:07 +0100

    Switch to a monadic approach

--- src/main/scala/microc/interpreter/BasicInterpreter.scala
+++ src/main/scala/microc/interpreter/BasicInterpreter.scala
@@ -2,134 +2,102 @@ package microc.interpreter
 
 import microc.analysis.Declarations
 import microc.ast._
+import microc.util.ErrorState
+import microc.util.ErrorState.{ErrorStateOps, put, crash => _crash, get => _get, pure => _pure}
 
 import java.io.{Reader, Writer}
 
 class BasicInterpreter(program: Program, declarations: Declarations, stdin: Reader, stdout: Writer)
   extends Interpreter {
 
-  object Context {
-    def pure[A](x: A): Context[A] = ValidContext(x, Array(), Map())
-    def sequence[A](xs: List[Context[A]]): Context[List[A]] = xs.foldLeft(pure(List())) { (acc, ctx) =>
-//      acc.flatMap(xs => ctx.map(x => x :: xs))
-      ???
-    }
+  type Context[A] = ErrorState[(String, Span), InterpreterState, A]
+  type Env = Map[Identifier, Int]
+  type Heap = Array[Value]
+
+  case class InterpreterState(heap: Heap, env: Env)
+
+  def get: Context[InterpreterState] = _get
+  def crash[A](e: String, sp: Span): Context[A] = _crash((e, sp))
+  def pure[A](x: A): Context[A] = _pure(x)
+
+  def heap: Context[Heap] = for (InterpreterState(heap, _) <- get) yield heap
+  def env: Context[Env] = for (InterpreterState(_, env) <- get) yield env
+  def setHeap(heap: Heap): Context[Unit] = for (s <- get) yield put(s.copy(heap = heap))
+  def setEnv(env: Env): Context[Unit] = for (s <- get) yield put(s.copy(env = env))
+
+  // TODO: what should dereferencing a function mean?
+  def deref(v: Value): Context[Option[Value]] = v match {
+    case AddrVal(addr) => for (h <- heap) yield Some(h(addr))
+    case FunAddrVal(name) => ???
+    case _ => pure(None)
   }
 
-  sealed trait Context[+A] {
-    import Context.sequence
+  def alloc(obj: Value): Context[Int] = for (h <- heap; _ <- setHeap(h :+ obj)) yield h.length
+  def bind(id: Identifier, addr: Int): Context[Unit] = for (e <- env; _ <- setEnv(e + ((id, addr)))) yield ()
+  def lookup(id: Identifier): Context[Int] = (for (e <- env) yield e.get(id) match {
+    case Some(addr) => pure(addr)
+    case None => crash(s"undefined ${id.name}", id.span)
+  }).flatten
 
-    def heap: Array[Value]
-    def env: Map[Identifier, Int]
-    def v: A
-
-    def map[B](f: A => B): Context[B] = this match {
-      case ctx@ValidContext(_, _, _) => ctx.copy(v = f(v))
-      case err: ErrorContext => err
-    }
-
-    def flatMap[B](f: A => Context[B]): Context[B] = this match {
-      case err: ErrorContext => err
-      case ValidContext(v, heap, env) =>
-        f(v) match {
-          case err: ErrorContext => err
-          case ValidContext(newV, newHeap, newEnv) =>
-            ValidContext(v = newV, heap = heap ++ newHeap, env = env ++ newEnv)
+  def eval(expr: Expr): Context[Value] = expr match {
+    case Null(_) => ???
+    case Number(value, _) => pure(IntVal(value))
+    case id@Identifier(_, _) => for (addr <- lookup(id); h <- heap) yield h(addr)
+    case BinaryOp(operator, left, right, _) => binOp(operator, left, right)
+    case CallFuncExpr(targetFun, argExprs, span) => (for (fnAddr <- eval(targetFun); fn <- deref(fnAddr)) yield fn match {
+      case addr: FunAddrVal =>
+        program.declarations.filter(_.isInstanceOf[FunDecl]).find(_.name == addr.name) match {
+          case None => crash(s"attempt to call undefined function ${addr.name}", span.highlighting(targetFun.span))
+          case Some(decl: FunDecl) =>
+            val ctx = argExprs.foldLeft(pure(List[Value]())) { case (ctx, argExp) =>
+              // FIXME ugly
+//              val xs = ctx.v
+//              for (x <- ctx.eval(argExp)) yield x :: xs
+              ???
+            }
+            // FIXME what is the receiver in the following call() call?
+            for (args <- ctx) yield call(decl, args, span)
         }
+      case _ => crash(s"expected a function address, got " /*FIXME*/, targetFun.span)
+    }).flatten.flatten
+    case Input(_) => ???
+    case Alloc(expr, _) => ???
+    case VarRef(id, _) => for (e <- env) yield AddrVal(e(id))
+    case Deref(pointer, span) => (for (addr <- eval(pointer); v <- deref(addr)) yield v match {
+      case Some(value) => pure(value)
+      case None => crash(s"cannot dereference $v", span)
+    }).flatten
+    case Record(fields, _) => ???
+    case FieldAccess(record, field, _) => ???
+  }
+
+  def call(f: FunDecl, args: List[Value], span: Span): Context[Value] = {
+    val bindings = for (
+      (decl, arg) <- f.params.zip(args)
+    ) yield (Identifier(decl.name, decl.span), arg)
+    val bound = bindings.foldLeft(pure(())) { case (acc, (id, arg)) =>
+      acc.flatMap(_ => alloc(arg).flatMap(addr => bind(id, addr)))
     }
+    f.block.vars
+    ???
+  }
 
-    def withFilter(f: A => Boolean): Context[A] = this match {
-      case ctx@ValidContext(v, _, _) if f(v) => ctx
-      case _: ValidContext[A] => crash(???, ???)
-      case err: ErrorContext => err
-    }
-
-    // TODO should insert match on `this` too?
-    def insert[B](x: B): Context[B] = ValidContext(v = x, heap = heap, env = env)
-
-    def crash(msg: String, span: Span): ErrorContext = ErrorContext(msg, span, heap, env)
-
-    def deref(expr: Expr): Context[Value] = for (addr <- eval(expr); v <- deref(addr, expr.span)) yield v
-    def deref(v: Value, span: Span): Context[Value]
-
-    def alloc(obj: Value): Context[Int] = this match {
-      case ValidContext(_, heap, env) => ValidContext(heap.length, heap :+ obj, env)
-      case err: ErrorContext => err
-    }
-
-    def bind(id: Identifier, addr: Int): Context[()] = this match {
-      case ValidContext(_, heap, env) => ValidContext((), heap, env + (id, addr))
-      case err: ErrorContext => err
-    }
-
-    def eval(expr: Expr): Context[Value] = expr match {
-      case Null(_) => ???
-      case Number(value, _) => insert(IntVal(value))
-      case id@Identifier(_, _) => insert(heap(env(id)))
-      case BinaryOp(operator, left, right, _) => binOp(operator, left, right)
-      case CallFuncExpr(targetFun, argExprs, span) => (for (fn <- deref(targetFun)) yield fn match {
-        case addr: FunAddrVal =>
-          program.declarations.filter(_.isInstanceOf[FunDecl]).find(_.name == addr.name) match {
-            case None => crash(s"attempt to call undefined function ${addr.name}", span.highlighting(targetFun.span))
-            case Some(decl: FunDecl) =>
-              val ctx = argExprs.foldLeft(insert(List[Value]())) { case (ctx, argExp) =>
-                // FIXME ugly
-                val xs = ctx.v
-                for (x <- ctx.eval(argExp)) yield x :: xs
-              }
-              // FIXME what is the receiver in the following call() call?
-              for (args <- ctx) yield call(decl, args, span)
-          }
-        case _ => crash(s"expected a function address, got $v", targetFun.span)
-      }).flatMap(identity).flatMap(identity)
-      case Input(_) => ???
-      case Alloc(expr, _) => ???
-      case VarRef(id, _) => insert(AddrVal(env(id)))
-      case Deref(pointer, span) => for (addr <- eval(pointer); value <- deref(addr, span)) yield value
-      case Record(fields, _) => ???
-      case FieldAccess(record, field, _) => ???
-    }
-
-    def call(f: FunDecl, args: List[Value], span: Span): Context[Value] = {
-      val bindings = for (
-        (decl, arg) <- f.params.zip(args)
-      ) yield (Identifier(decl.name, decl.span), arg)
-      val bound = bindings.foldLeft(this.insert(())) { case (acc, (id, arg)) =>
-        acc.flatMap(_ => alloc(arg).flatMap(addr => bind(id, addr)))
+  def binOp(operator: BinaryOperator, left: Expr, right: Expr): Context[Value] = {
+    for (lv <- eval(left); rv <- eval(right))
+      yield (lv, rv) match {
+        case (IntVal(l), IntVal(r)) =>
+          pure(IntVal(operator match {
+            case Plus => l + r
+            case Minus => l - r
+            case Times => l * r
+            case Divide => l / r
+            case Equal => if (l == r) 1 else 0
+            case LessThan => if (l < r) 1 else 0
+          }))
+        case (IntVal(_), problem) => crash(s"operator $operator expected int, got $problem", right.span)
+        case (problem, _) => crash(s"operator $operator expected int, got $problem", left.span)
       }
-      f.block.vars
-    }
-
-    def binOp(operator: BinaryOperator, left: Expr, right: Expr): Context[Value] = {
-      for (lv <- eval(left); rv <- eval(right))
-        yield (lv, rv) match {
-          case (IntVal(l), IntVal(r)) =>
-            insert(IntVal(operator match {
-              case Plus => l + r
-              case Minus => l - r
-              case Times => l * r
-              case Divide => l / r
-              case Equal => if (l == r) 1 else 0
-              case LessThan => if (l < r) 1 else 0
-            }))
-          case (IntVal(_), problem) => crash(s"operator $operator expected int, got $problem", right.span)
-          case (problem, _) => crash(s"operator $operator expected int, got $problem", left.span)
-        }
-    }.flatMap(identity)
-  }
-
-  case class ValidContext[A](v: A, heap: Array[Value], env: Map[Identifier, Int]) extends Context[A] {
-    override def deref(v: Value, span: Span): Context[Value] = v match {
-      case AddrVal(addr) => insert(heap(addr))
-      // TODO functions?
-      case value => crash(s"attempt to dereference $value", span)
-    }
-  }
-
-  case class ErrorContext(msg: String, span: Span, heap: Array[Value], env: Map[Identifier, Int]) extends Context[Nothing] {
-    def v: Nothing = ???
-    override def deref(v: Value, s: Span): Context[Value] = ???
-  }
+  }.flatten
 
   sealed trait Value
   case class IntVal(n: Int) extends Value
@@ -139,10 +107,10 @@ class BasicInterpreter(program: Program, declarations: Declarations, stdin: Read
 
   def run(mainArgs: List[Int] = Nil): Int = program.declarations.find(_.name == "main") match {
     case Some(decl@FunDecl(name, params, block, span)) =>
-      Context.pure(()).call(decl, mainArgs.map(IntVal), span) match {
-        case ValidContext(IntVal(n), _, _) => n
-        case ValidContext(v, _, _) => throw ExecutionException(s"expected an integer, but main returned $v", span)
-        case ErrorContext(msg, span, _, _) => throw ExecutionException(msg, span)
+      call(decl, mainArgs.map(IntVal), span)(InterpreterState(Array(), Map())) match {
+        case Right((IntVal(n), _)) => n
+        case Right((v, _)) => throw ExecutionException(s"expected an integer, but main returned $v", span)
+        case Left((msg, span)) => throw ExecutionException(msg, span)
       }
     case _ => throw ExecutionException("undefined entry point", program.span)
   }
---
+++ src/main/scala/microc/util/ErrorState.scala
@@ -0,0 +1,36 @@
+package microc.util
+
+import microc.util.ErrorState.flatMap
+
+trait ErrorState[Error, State, +A] {
+  def apply(s: State): Either[Error, (A, State)]
+  def flatten[B](implicit ev: A <:< ErrorState[Error, State, B]): ErrorState[Error, State, B] = flatMap(this)(x => x)
+}
+
+object ErrorState {
+  // aka return
+  def pure[E, S, A](a: A): ErrorState[E, S, A] = s => Right((a, s))
+  // aka bind aka >>=
+  def flatMap[E, S, A, B](x: ErrorState[E, S, A])(f: A => ErrorState[E, S, B]): ErrorState[E, S, B] =
+    s => x(s) match {
+      case Left(err) => Left(err)
+      case Right((a, s2)) => f(a)(s2)
+    }
+
+  def get[E, S]: ErrorState[E, S, S] = s => Right((s, s))
+  def put[E, S](s: S): ErrorState[E, S, Unit] = _ => Right(((), s))
+  def crash[E, S, A](e: E): ErrorState[E, S, A] = _ => Left(e)
+
+  implicit class ErrorStateOps[E, S, A](x: ErrorState[E, S, A]) {
+    def flatMap[B](f: A => ErrorState[E, S, B]): ErrorState[E, S, B] = ErrorState.flatMap(x)(f)
+    def map[B](f: A => B): ErrorState[E, S, B] = x.flatMap(a => pure(f(a)))
+    def withFilter(f: A => Boolean): ErrorState[E, S, A] = x.flatMap(a => () match {
+      case _ if f(a) => pure(a)
+      case _ => crash(???)
+    })
+  }
+
+  implicit class ErrorStateFlattenOps[E, S, A](x: ErrorState[E, S, ErrorState[E, S, A]]) {
+    def flatten: ErrorState[E, S, A] = flatMap(x)(identity)
+  }
+}
