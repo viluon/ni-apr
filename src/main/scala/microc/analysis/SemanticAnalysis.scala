ae914862b1881e0a855aeca77a8e34e44e2b93f2
Andrew Kvapil <viluon@pm.me>
2022-05-17 20:37:19 +0200

    Fix scoping issues

--- src/main/scala/microc/analysis/SemanticAnalysis.scala
+++ src/main/scala/microc/analysis/SemanticAnalysis.scala
@@ -49,16 +49,6 @@ class SemanticAnalysis {
 //    println("\t" + state.env)
   }
 
-  // fixme: redundant
-  private def addDecls(d: Declarations): Analysis[Unit] =
-    get.flatMap(state => d.foldLeft(pure(state.decls)) {
-      case (ctx, kv@(id, decl)) => ctx.flatMap(acc => acc.get(id) match {
-        case Some(previousDecl) =>
-          crash(s"redeclaration of ${decl.name}, previously declared at ${previousDecl.span}", decl.span)
-        case _ => pure(acc + kv)
-      })
-    }.flatMap(declarations))
-
   def combine(saved: Env, e: Iterable[(String, Decl)]): Analysis[Env] = e.foldLeft(pure(saved)) {
     case (ctx, kv@(name, decl)) => ctx.flatMap(acc => acc.get(name) match {
       case Some(previousDecl) => crash(s"redeclaration of ${decl.name}, previously declared at ${previousDecl.span}", decl.span)
@@ -66,13 +56,13 @@ class SemanticAnalysis {
     })
   }
 
-  private def inScope[A](e: Iterable[(String, Decl)])(m: Analysis[A]): Analysis[A] = for (
-    saved <- env;
-    updated <- combine(saved, e);
-    () <- setEnv(updated);
-    a <- m;
+  private def inScope[A](e: Iterable[(String, Decl)])(m: Analysis[A]): Analysis[A] = for {
+    saved <- env
+    updated <- combine(saved, e)
+    () <- setEnv(updated)
+    a <- m
     () <- setEnv(saved)
-  ) yield a
+  } yield a
 
   /**
     * Catch semantic errors in the given program.
@@ -85,34 +75,34 @@ class SemanticAnalysis {
     case Right((_, state)) => (state.decls, state.fieldNames)
   }
 
-  private def go(program: Program): Analysis[Unit] = for (
-    fnDecls <- combine(Map(), program.funs.map(f => (f.name, f: Decl)));
-    () <- setEnv(fnDecls);
+  private def go(program: Program): Analysis[Unit] = for {
+    fnDecls <- combine(Map(), program.funs.map(f => (f.name, f: Decl)))
+    () <- setEnv(fnDecls)
     () <- reduce(program.funs) { decl =>
-      inScope(decl.params.map(id => (id.name, id: Decl)))(for (
-        () <- debug(s"function ${decl.name}");
+      inScope(decl.params.map(id => (id.name, id: Decl)))(for {
+        () <- debug(s"function ${decl.name}")
         () <- go(decl.block)
-      ) yield ())
+      } yield ())
     }
-  ) yield ()
+  } yield ()
 
   @tailrec
   private def lvalue(expr: Expr): Analysis[Boolean] = expr match {
-    case ast.Null(span) => pure(false)
-    case ast.Number(value, span) => pure(false)
-    case Identifier(name, span) => env.flatMap(_.get(name) match {
+    case ast.Null(_) => pure(false)
+    case ast.Number(_, _) => pure(false)
+    case Identifier(name, _) => env.flatMap(_.get(name) match {
       case Some(_: FunDecl) => crash(s"cannot assign to a function", expr.span)
       // not handling unbound identifiers here, regular analysis should catch those
       case _ => pure(true)
     })
-    case BinaryOp(operator, left, right, span) => pure(false)
-    case CallFuncExpr(targetFun, args, span) => pure(false)
-    case Input(span) => pure(false)
-    case Alloc(expr, span) => pure(false)
-    case VarRef(id, span) => pure(false)
-    case Deref(pointer, span) => lvalue(pointer)
-    case Record(fields, span) => pure(false)
-    case FieldAccess(record, field, span) => lvalue(record)
+    case BinaryOp(_, _, _, _) => pure(false)
+    case CallFuncExpr(_, _, _) => pure(false)
+    case Input(_) => pure(false)
+    case Alloc(_, _) => pure(false)
+    case VarRef(_, _) => pure(false)
+    case Deref(pointer, _) => lvalue(pointer)
+    case Record(_, _) => pure(false)
+    case FieldAccess(record, _, _) => lvalue(record)
   }
 
   private def expect(a: Boolean): (String, Span) => Analysis[Unit] =
@@ -125,12 +115,12 @@ class SemanticAnalysis {
         case Some(_: FunDecl) => crash("cannot assign to a function", span.highlighting(id.span))
         case None => go(id) // this call will crash and take care of the error message
       })
-      case AssignStmt(left, right, span) => for (
-        isLvalue <- lvalue(left);
-        () <- expect(isLvalue)(s"cannot assign to rvalue $left", span.highlighting(left.span));
-        () <- go(left);
+      case AssignStmt(left, right, span) => for {
+        isLvalue <- lvalue(left)
+        () <- expect(isLvalue)(s"cannot assign to rvalue $left", span.highlighting(left.span))
+        () <- go(left)
         () <- go(right)
-      ) yield ()
+      } yield ()
       case NestedBlockStmt(body, _) => goOver(body)
       case IfStmt(guard, thenBranch, elseBranch, _) =>
         for (() <- go(guard); () <- go(thenBranch); () <- elseBranch.map(go).getOrElse(pure(()))) yield ()
@@ -140,10 +130,10 @@ class SemanticAnalysis {
     case block: Block => block match {
       case NestedBlockStmt(body, _) => goOver(body)
       case FunBlockStmt(vars, stmts, ret, _) =>
-        inScope(vars.flatMap(v => v.decls).map(id => (id.name, id: Decl)))(for (
-          () <- debug("function body");
+        inScope(vars.flatMap(v => v.decls).map(id => (id.name, id: Decl)))(for {
+          () <- debug("function body")
           () <- goOver(stmts :+ ret)
-        ) yield ())
+        } yield ())
     }
     case ReturnStmt(expr, _) => go(expr)
     case VarStmt(_, _) => throw new IllegalStateException("VarStmt must be handled by the enclosing function")
@@ -151,15 +141,19 @@ class SemanticAnalysis {
 
   @tailrec
   private def go(expr: Expr): Analysis[Unit] = expr match {
-    case ast.Null(_) => debug("expr").flatMap(_ => addDecls(Map()))
-    case ast.Number(_, _) => debug("expr").flatMap(_ => addDecls(Map()))
+    case ast.Null(_) => debug("expr")
+    case ast.Number(_, _) => debug("expr")
     case id@Identifier(name, span) => env.flatMap(_.get(name) match {
-      case Some(decl) => debug("expr").flatMap(_ => addDecls(Map(id -> decl)))
+      case Some(decl) => for {
+        _ <- debug("expr")
+        s <- get
+        _ <- put(s.copy(decls = s.decls + (id -> decl)))
+      } yield ()
       case None => crash(s"undefined reference to $name", span)
     })
     case BinaryOp(_, left, right, _) => goOverExprs(List(left, right))
     case CallFuncExpr(targetFun, args, _) => goOverExprs(targetFun :: args)
-    case Input(_) => debug("expr").flatMap(_ => addDecls(Map()))
+    case Input(_) => debug("expr")
     case Alloc(expr, _) => go(expr)
     case VarRef(id, _) => go(id)
     case Deref(pointer, _) => go(pointer)
